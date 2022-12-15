7b31bdd1340c7a72440a6c15f01e46ee07a8e25c
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-12-15 01:45:30 -0800

    add vcpkg to example

---
+++ examples/ports/cppdap/portfile.cmake
@@ -0,0 +1,83 @@
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
+#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#   VCPKG_TOOLCHAIN           = ON OFF
+#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
+#   BUILD_ARCH                = "Win32" "x64" "ARM"
+#   MSBUILD_PLATFORM          = "Win32"/"x64"/${TRIPLET_SYSTEM_ARCH}
+#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
+#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
+#   VCPKG_TARGET_IS_WINDOWS
+#   VCPKG_TARGET_IS_UWP
+#   VCPKG_TARGET_IS_LINUX
+#   VCPKG_TARGET_IS_OSX
+#   VCPKG_TARGET_IS_FREEBSD
+#   VCPKG_TARGET_IS_ANDROID
+#   VCPKG_TARGET_IS_MINGW
+#   VCPKG_TARGET_EXECUTABLE_SUFFIX
+#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
+#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
+#
+# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
+
+
+# vcpkg_from_github(
+#   OUT_SOURCE_PATH SOURCE_PATH
+#   REPO nikitalita/cppdap
+#   REF 1.46.0-cmake
+#   SHA512 e4f28af549f01a9702023b94713b0051f9a468ca25b39557392636d427a8cb32be3a8464c9247ebbd46122c4c97bed38012461f04aec9610381f917ddce60343
+#   HEAD_REF cmake-install
+# )
+# vcpkg_from_github() does not recurse submodules
+find_program(GIT git)
+
+set(GIT_URL "https://github.com/nikitalita/cppdap.git")
+set(GIT_REV "1.46.0-cmake")
+
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
+
+file(MAKE_DIRECTORY "${SOURCE_PATH}")
+if(NOT EXISTS "${SOURCE_PATH}/.git")
+	message(STATUS "Cloning and fetching submodules")
+	vcpkg_execute_required_process(
+	  COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
+	  WORKING_DIRECTORY ${SOURCE_PATH}
+	  LOGNAME clone
+	)
+
+	message(STATUS "Checkout revision ${GIT_REV}")
+	vcpkg_execute_required_process(
+	  COMMAND ${GIT} checkout ${GIT_REV}
+	  WORKING_DIRECTORY ${SOURCE_PATH}
+	  LOGNAME checkout
+	)
+endif()
+# # Check if one or more features are a part of a package installation.
+# # See /docs/maintainers/vcpkg_check_features.md for more details
+# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+#   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
+#     tbb   WITH_TBB
+#   INVERTED_FEATURES
+#     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
+# )
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME cppdap CONFIG_PATH lib/cmake/cppdap)
+file(
+        INSTALL "${SOURCE_PATH}/LICENSE"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+        RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
